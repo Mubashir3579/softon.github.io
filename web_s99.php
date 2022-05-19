f79d5a4b0cc94ed33dd6ad9461ee04e314588ade
Mubashir3579 <mubashir.arshad1702@gmail.com>
2022-05-19 07:46:39 +0500

    my Softon website

---
+++ web_s99.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <html lang="en"><link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <meta charset="utf-8">
+        <title></title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="Free Website Template" name="keywords">
+        <meta content="Free Website Template" name="description">
+        
+             
+        
+                <!-- Favicon 
+                <link href="img/favicon.ico" rel="icon">   -->
+        
+                 <!-- Google Font -->
+                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+        
+                 <!-- CSS Libraries -->
+                 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+                 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+                 <link href="lib/animate/animate.min.css" rel="stylesheet">
+                 <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+                
+         
+                 <!-- Template Stylesheet -->
+              <link rel="stylesheet" href="css/style.css">
+
+              <style> 
+
+                
+                .pp {   font-style: italic  ;
+                        background:white;
+                    margin: 5px 0 22px 0;   
+                  width: 95%;
+                padding: 10px;
+                display: block;
+                border: none; 
+                position: relative;
+                left: 2%;
+                color: black;
+                z-index: 1;
+               
+                border-width: 10px;
+                }
+                .pp:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+            .killer { 
+                font-weight: 10nm; 
+                    color: black ;
+                    font-size: 22px;
+                border-radius: 5px;
+  
+  padding: 20px;
+font-size: 20 px;}
+.pika {
+    border-radius: 5px;
+            padding:15px;
+            background-color: #e8ebe4;
+            opacity: 0.7        ;
+                width: 60%;
+    position: relative;
+    left: 20%;
+}
+            </style>
+
+                
+    </head>
+
+    <body  style="background-image:url(bbb.jpg)"  >
+        <!-- Top Bar Start -->
+      <!-- Top Bar Start -->
+      <div class="top-bar d-none d-md-block">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="top-bar-left">
+                        <div class="text">
+                                                <!---->
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="top-bar-right">
+                        <div class="social">
+                            <a href=""><i class="fab fa-twitter"></i></a>
+                            <a href=""><i class="fab fa-facebook-f"></i></a>
+                            <a href=""><i class="fab fa-linkedin-in"></i></a>
+                            <a href=""><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Top Bar End -->
+
+    <!-- Nav Bar Start -->
+    <div class="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div class="container-fluid">
+            <a href="index.html" class="navbar-brand">  𝔰𝔬𝔣𝔱𝔬𝔫
+              <!--  <img  src="C:\Users\Bullet\Desktop\sh\img\logo.png"class="fifa" 
+>  -->  <a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                <div class="navbar-nav ml-auto">
+                    <a href="index.html" class="nav-item nav-link " id="myMenu"  >Home</a>
+                    <a href="web_s2-p.html" class="nav-item nav-link " id="myMenu" >Portfolio</a>
+                    <a href="aboutt.html" class="nav-item nav-link "  id="myMenu" >About</a>
+                    <div class="nav-item dropdown">
+                        <a href="Services.html" class="nav-link dropdown-toggle " data-toggle="dropdown"id="myMenu"  >Service</a>
+                        <div class="dropdown-menu">
+                            <!--sevicces add-->                
+                            <a href="Services.html" class="dropdown-item">Main Services</a>
+                            <a href="web_s8.html" class="dropdown-item">Graphics</a>
+                            <a href="web_s9.html" class="dropdown-item">Website designing</a>
+                            <a href="web_s2.html" class="dropdown-item">Application</a>
+                            <a href="web_s3.html" class="dropdown-item">Mobile Apps</a>
+                            <a href="web_s4.html" class="dropdown-item">Software</a>
+                        </div>
+                    </div>
+                    <div class="nav-item dropdown">
+                        <a href="#.html" class="nav-link dropdown-toggle" data-toggle="dropdown"id="myMenu"  >Review</a>
+                        <div class="dropdown-menu">
+                            <!--sevicces add-->                
+                            <a href="web_s1.html" class="dropdown-item">Application Types</a>
+                            <a href="web_s5.html" class="dropdown-item">System Software</a>
+                            <a href="web_s77.html" class="dropdown-item">Apps Development</a>
+                            
+                        </div>
+                    </div> 
+                    
+                    
+                    
+                    <a href="contacts.html" class="nav-item nav-link" id="myMenu" >Contact</a>
+                    <a href="http://localhost/web_s99.php" class="nav-item nav-link active" id="myMenu" >Sign Up</a>
+                   <!--searxh section start-->
+                     <form class="example" action="#" style="margin:auto;max-width:300px">
+                     <input type="text" placeholder="Search.." name="search2">
+                     <button type="submit"><i class="fa fa-search"></i></button>
+                    </form>
+
+                            
+
+
+
+                   <!--searxg section end -->
+
+                </div>
+            </div>
+        </div>
+    </div>
+      <!-- Service Start -->
+    <!-- Nav Bar End -->
+        <!--style start here-->
+
+        <!--style start here-->
+
+        
+
+
+
+        <!--style end here-->
+       
+
+
+
+      
+
+
+  <pre>
+
+
+
+  </pre>      <!-- Service Start -->
+<div  class="killer" >
+
+
+<h2 style =text-align:center; > Sign up Form </h2>
+<form   class="pika" action="insert.php"  method="post">
+
+
+
+    <label>Name :</label>
+    <input    class="pp" type="text" name="username"  placeholder="Username"  required >
+
+    <label>Password :</label>
+    <input class="pp" type="text" name="password"  placeholder="Password" required>
+<label>Gender :
+        <input  type="radio" name="gender" value="m" required>Male 
+        <input  type="radio" name="gender" value="f" required  >Female </label>
+<br>
+<br>  
+
+<label> Email :</label>
+    <input  class="pp"type="email" name="email" required >
+<label> Phone :</label>
+
+     <select class="pp" name="phoneCode"required >
+        <option  class="pp"  value="030">030</option>
+        <option class="pp" value="031">031</option>
+        <option class="pp" value="032">032</option>
+        <option class="pp" value="033">033</option>
+        <option class="pp" value="034">034</option>
+    </select>
+    <input class="pp" type="phone" name="phone" required>
+
+<input class="pp" type="submit" value="Submit">
+
+
+
+
+</form>
+
+
+</div>
+
+
+
+
+        <!-- Service End -->
+
+
+        <!-- Footer Start -->
+        <div class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="footer-contact">
+                                    <h2>Address</h2>
+                                    <p><i class="fa fa-map-marker-alt"></i>123 Street, New York, USA</p>
+                                    <p><i class="fa fa-phone-alt"></i>+012 345 67890</p>
+                                    <p><i class="fa fa-envelope"></i>info@example.com</p>
+                                    <div class="footer-social">
+                                        <a href=""><i class="fab fa-twitter"></i></a>
+                                        <a href=""><i class="fab fa-facebook-f"></i></a>
+                                        <a href=""><i class="fab fa-youtube"></i></a>
+                                        <a href=""><i class="fab fa-instagram"></i></a>
+                                        <a href=""><i class="fab fa-linkedin-in"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="footer-link">
+                                    <h2>Quick Links</h2>
+                                    <a href="">Services</a>
+                                    <a href="">About</a>
+                                    <a href="">Contact</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-5">
+                        <div class="footer-newsletter">
+                            <h2>Feedback</h2>
+                            <div class="form">
+                                <input class="form-control" placeholder="Feedback Here">
+                                <div>
+                                    <input class="form-control" placeholder="Gmail Here">
+                                </div>
+                                <div>
+                                    <input class="btnn" type="button" value="Summit">
+                                </div>
+                            </div>
+                           
+                            <div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <!-- Footer End -->
+
+        <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+        <!-- JavaScript Libraries -->
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+        <script src="lib/easing/easing.min.js"></script>
+        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+        <script src="lib/isotope/isotope.pkgd.min.js"></script>
+        <script src="lib/lightbox/js/lightbox.min.js"></script>
+        
+        <!-- Contact Javascript File -->
+        <script src="mail/jqBootstrapValidation.min.js"></script>
+        <script src="mail/contact.js"></script>
+
+        <!-- Template Javascript -->
+        <script src="js/main.js"></script>
+    </body>
+</html>
